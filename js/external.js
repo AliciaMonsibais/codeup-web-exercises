eab702f6e29aec9c3c3cb906e074938643d64668
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-12 16:39:16 -0500

    Adding training wheels of JS

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log('Hello from external Javascript!');
+
+alert("Welcome to my website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log("The user's favorite color is " + userInput);
+var confirmed = confirm("Great, " + userInput + " is my favorite color too!");
