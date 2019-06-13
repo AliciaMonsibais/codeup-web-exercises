43576c9839349cb0323adf2d1ea139eab23e6959
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-13 09:41:40 -0500

    Update with more prompts

--- js/external.js
+++ js/external.js
@@ -6,7 +6,11 @@ alert("Welcome to my website!");
 
 var userInput = prompt('What is your favorite color?');
 console.log("The user's favorite color is " + userInput);
-var confirmed = confirm("Great, " + userInput + " is my favorite color too!");
+if (userInput === "yellow", "Yellow") {
+    confirm("Yellow? Um... okay.");
+} else {
+    confirm("Great, " + userInput + " is my favorite color too!");
+}
 
 //exercise #3
 
