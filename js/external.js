0953b1313a091dc1aad42d2001b1240b90f5a0ca
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-13 09:16:36 -0500

    Updated with prompt exercises.

--- js/external.js
+++ js/external.js
@@ -18,7 +18,7 @@ var bear = prompt("How many days will you rent Brother Bear?");
 console.log("The user will rent Brother Bear for " + bear + " days.");
 var hercules = prompt("How many days will you rent Hercules?");
 console.log("The user will rent Hercules for " + hercules + " days.");
-var movieTotal = confirm("Your total will be " + ((mermaid * 3) + (bear * 3) + (hercules * 3)));
+var movieTotal = confirm("Your total will be $" + ((mermaid * 3) + (bear * 3) + (hercules * 3)));
 
 //pay rate
 
@@ -38,3 +38,30 @@ var facebook = prompt("How much does Facebook pay per hour?");
 console.log("The user makes " + facebook + " per hour with Facebook.");
 var facebookHours = prompt("How many hours did you work for Facebook this week?")
 var contractTotal = confirm("You will make $" + ((google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours)) + " this week.");
+
+//class
+
+var studentTime = prompt("What time are you looking for this class to be available?");
+if (studentTime === "11:30") {
+    alert("Fantastic! There's a class available at that time.");
+} else if (studentTime === "1337") {
+    alert("Leet!");
+} else if (studentTime === "13:37") {
+    alert("Leet!");
+} else {
+    alert(studentTime + " isn't available for your desired course. Shucks.");
+}
+
+//product offer
+
+var premium = confirm("Are you a member of our super awesome program?");
+    if (premium) {
+        alert("Fantastic! Enjoy this nifty item with no purchase necessary!");
+    } else {
+       var numberOfItems= prompt("You can receive a promo item! But first, how many items have you bought?");
+    }
+    if (numberOfItems > 2) {
+        alert("Great! Enjoy this promo item.");
+    } else {
+        alert("Please purchase 2 or more items to receive this super cool promo item.");
+    }
