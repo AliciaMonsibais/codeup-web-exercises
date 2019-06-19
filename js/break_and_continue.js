fbc4dd7d0e95e0a521f0230f6e708c98e8d7d505
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-19 12:25:34 -0500

    Adding updated exercises for loops.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,3 +1,34 @@
 "use strict";
 
 /**Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.*/
+
+(function() {
+    var numberPicked;
+    do {
+        numberPicked = Number(prompt("Pick an odd number between 1 and 50."));
+        if (numberPicked < 1 || numberPicked > 50)
+            alert(numberPicked + " is not between 1 and 50.");
+        else if (numberPicked % 2 === 0)
+            alert(numberPicked + " is not odd, please pick again.");
+        else if (isNaN(numberPicked))
+            alert (numberPicked + " is not a number.");
+        else {
+            alert("Congrats! You can follow directions.");
+            break;
+        }
+    } while (true);
+
+
+/**Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.*/
+//+= automatically makes the counting in odd increments rather than even//
+
+    var yikes = function(skip){
+        for (var i = 1; i < 50; i += 2) {
+            if (i === skip) {
+                console.log("Yikes! Skipping number: " + i);
+                continue;
+            } console.log("Here is an odd number: " + i);
+        }
+    };
+    yikes(numberPicked);
+})();
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,40 +1,46 @@
 "use strict";
 
-// /**Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)*/
-// (function showMultiplicationTable(){
-//     var x = Math.floor((Math.random() * 10) + 1);
-//     for(var i = 1; i < 10; i++){
-//         console.log(x * i);
-//     }
-// })();
-//
-// /**Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.*/
-// (function isOdd(){
-//     var i = 0;
-//     var randoNumber = (Math.floor(Math.random() * 180) + 20);
-//     if (randoNumber % 2 === 0){
-//         return randoNumber + " is even.";
-//     } else {
-//         return randoNumber + " is odd.";
-//     // }
-//     for(i = 0; i < 10; i += 1);
-//         if (randoNumber % 2 === 0) {
-//             console.log(randoNumber + " is even.");
-//         } else {
-//             console.log(randoNumber + " is odd.");
-//     }
-// })();
+/**Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)*/
+(function (){
+    var showMultiplicationTable = function(x){
+
+    var x = Math.floor((Math.random() * 10) + 1);
+    for(var i = 1; i <= 10; i++){
+        console.log(x + " x " + i + " = " + (i*parseInt(x)));
+    }
+    }; showMultiplicationTable(Math.floor((Math.random() * 10) + 1))
+})();
+
+/**Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.*/
+(function (){
+    for(var i = 0; i < 10; i ++) {
+        var randoNumber = (Math.floor(Math.random() * 180) + 20);
+        var x = randoNumber % 2;
+        if (x === 0) {
+            console.log(randoNumber + " is even.");
+        } else {
+            console.log(randoNumber + " is odd.");
+        }
+    }
+})();
 
 /**Create a for loop that uses console.log to create the output shown below.*/
-
-for (var i = 1; i < 10; i++) {
-    console.log(i.repeat(3));
-}
+(function(){
+var christmasTree = function() {
+    for (var outer = 1; outer <= 9; outer++) {
+        var output = '';
+        for (var inner = 1; inner <= outer; inner++) {
+            output += outer;
+        }
+        console.log(output);
+    }
+};
+christmasTree();
+})();
 
 /**Create a for loop that uses console.log to create the output shown below.*/
-// (function lessThanFive(){
-//     for (var i = 100; i > 0; i--){
-//         console.log(i);
-//     }
-// })();
-
+(function lessThanFive(){
+    for (var i = 100; i >= 5; i -= 5){
+        console.log(i);
+    }
+})();
