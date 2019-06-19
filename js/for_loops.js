1ba58cfe62474ab250e984c4087f1b164d747572
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-19 11:02:54 -0500

    updating with loop exercise WIP

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,30 +1,40 @@
 "use strict";
 
-/**Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)*/
-(function showMultiplicationTable(){
-    var x = Math.floor((Math.random() * 10) + 1);
-    for(var i = 1; i < 10; i++){
-        console.log(x * i);
-    }
-})();
-
-/**Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.*/
-(function isOdd(){
-    var randoNumber = (Math.floor(Math.random() * 180) + 20);
-    if (randoNumber % 2 === 0){
-        return randoNumber + " is even.";
-    } else {
-        return randoNumber + " is odd.";
-    }
-})();
+// /**Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)*/
+// (function showMultiplicationTable(){
+//     var x = Math.floor((Math.random() * 10) + 1);
+//     for(var i = 1; i < 10; i++){
+//         console.log(x * i);
+//     }
+// })();
+//
+// /**Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.*/
+// (function isOdd(){
+//     var i = 0;
+//     var randoNumber = (Math.floor(Math.random() * 180) + 20);
+//     if (randoNumber % 2 === 0){
+//         return randoNumber + " is even.";
+//     } else {
+//         return randoNumber + " is odd.";
+//     // }
+//     for(i = 0; i < 10; i += 1);
+//         if (randoNumber % 2 === 0) {
+//             console.log(randoNumber + " is even.");
+//         } else {
+//             console.log(randoNumber + " is odd.");
+//     }
+// })();
 
 /**Create a for loop that uses console.log to create the output shown below.*/
 
+for (var i = 1; i < 10; i++) {
+    console.log(i.repeat(3));
+}
 
 /**Create a for loop that uses console.log to create the output shown below.*/
-(function lessThanFive(){
+// (function lessThanFive(){
+//     for (var i = 100; i > 0; i--){
+//         console.log(i);
+//     }
+// })();
 
-    for (var i = 5; x = 100; i > 0; i++){
-        console.log(x - i);
-    }
-})();
--- js/while.js
+++ js/while.js
@@ -3,7 +3,7 @@
 /** Create a while loop that uses console.log() to create the output shown below.*/
 (function() {
     var x = 2;
-    while (x <= 65537) {
+    while (x <= 65536) {
         console.log(x);
         x *= 2
     }
@@ -12,21 +12,19 @@
 /**An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.*/
 
 (function(){
-    var moreCones = true;
     var cones = (Math.floor(Math.random() * 50) + 50);
     console.log("You need to sell " + cones + " cones.");
-    function gimme() {
-        var gimmeMore = Math.floor((Math.random() * 5) + 1);
-        cones -= gimmeMore;
-    }
     do {
-        gimme();
-        console.log("You have " + cones + " cones.");
-        if (cones <= 0){
-        moreCones =  false;
-        console.log("You're sold out! Go home.")
+        var gimmeMore = Math.floor((Math.random() * 5) + 1);
+
+        if (gimmeMore <= cones) {
+            cones -= gimmeMore;
+            console.log(gimmeMore + " cones sold...");
+        } else {
+            console.log ("Cannot sell you " + gimmeMore + " I only have " + cones + "...");
         }
-    } while(moreCones);
+    } while(cones > 0);
+    console.log("Yay! I sold them all!")
 })();
 
 
