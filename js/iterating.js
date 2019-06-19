945d92ccac65a52cc919c4469d6e8e8e4a31c6ce
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-19 16:09:06 -0500

    Adding arrays iterating exercise

---
+++ js/iterating.js
@@ -0,0 +1,69 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+    var names = ["Alicia", "Nicole", "Brandon", "Anthony"];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+    console.log("There are " + names.length + " names in the array.");
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+    //  */
+    // For loop://
+    //
+    // for (var i = 0; i < names.length; i++) {
+    //     console.log("Name at index: " + i + " is " + names[i]);
+    // }
+
+    //ForEach loop//
+
+    names.forEach(function (name) {
+        console.log("Here is a name: " + name);
+
+    });
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+    var coolArray = [1, 2, 3, 4, 5];
+    console.log("I have " + coolArray.length + " elements in my super cool Array!");
+    var first = coolArray[0];
+    console.log("Here is the first array item: " + first);
+    var second = coolArray[1];
+    console.log("Here is the second array item: " + second);
+    var last = coolArray[4];
+    console.log("Here is the last array item: " + last);
+
+})();
