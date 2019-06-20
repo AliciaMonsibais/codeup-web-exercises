b1bade96698e932d79d63e716a672717729db52b
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-06-20 09:58:51 -0500

    Adding planet arrays first exercise

--- js/bonus_arrays.js
+++ js/bonus_arrays.js
@@ -1,4 +1,21 @@
 (function(){
     "use strict";
 
+/**Array Bonuses (eyes emoji)
+ * - Define a function named `allIndexesOf` that takes in two arguments. The first
+ argument should be the array to search and the second argument should be the
+ value you want to search for. If the item does not exist in the provided
+ array, return an empty array.
+     ​
+ Given:
+     ​
+ ```js
+ var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+ ```
+     ​
+ - `allIndexesOf(fruits, "apple")` should return the array [0, 3]
+ - `allIndexesOf(fruits, "guava")` should return the array []
+ - `allIndexesOf(fruits, "pineapple")` should return [4]*/
+
+
 })();
