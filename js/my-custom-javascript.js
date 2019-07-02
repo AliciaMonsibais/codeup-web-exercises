4c420a1faba91df80df9ea6740dda0183f4ed567
Alicia Monsibais <alicia@Alicias-MacBook-Air.local>
2019-07-02 15:19:48 -0500

    working on jquery mouse events

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,7 +1,28 @@
 (function (){
 "use strict";
 
-$('li').css('font-size', '20px');
-$('h1, p, li').css('background-color', '#5fc2c2');
-alert($('h1').html());
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('background-color', '#5fc2c2');
+// alert($('h1').html());
+    $().ready(
+        function(){
+            $('h1').click (
+                function(e){
+                    $(this).css('background-color', '#5fc2c2')
+                }
+            );
+            $('p').dblclick(function (e){
+                $(this).css('font-size', '18px')
+                }
+            );
+            $('li').hover (
+                function(e){
+                    $(this).css('color', 'red')
+                },
+                function(e){
+                    $(this).css('color', 'black');
+                }
+            )
+        }
+    )
 }());
