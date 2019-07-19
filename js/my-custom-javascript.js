515dee072d2c5dde15c52423c1ea4d8ba7929839
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-07-19 09:05:19 -0500

    updating exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,11 @@
  $(document).ready(
-        // $('li').css('font-size', '20px');
-// $('h1, p, li').css('background-color', '#5fc2c2');
-// alert($('h1').html());
         function(){
+            // alert( 'The DOM has finished loading!' );
+            // $('.codeup').css('border', '1px solid red');
+            // $('li').css('font-size', '20px');
+
+            // alert($('h1').html());
+            // $('h1, p, li').css('background-color', '#5fc2c2');
             $('h1').click (
                 function(e){
                     $(this).css('background-color', '#5fc2c2')
